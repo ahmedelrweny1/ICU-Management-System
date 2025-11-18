320fd008a5e2eee2a073fbdbf0559d8590735d98
Ahmed Elrweny <elrwenyahmed05@gmail.com>
2025-11-18 13:25:00 +0200

    Add Home.Signup action

--- Shefaa-ICU/Controllers/HomeController.cs
+++ Shefaa-ICU/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace Shefaa_ICU.Controllers
             return View();
         }
 
+        public IActionResult Signup()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
